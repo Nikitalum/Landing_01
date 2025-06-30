cf37ba45606a2a482517363058bad771af099ad3
Nikitalum <chesterryos@gmail.com>
2025-06-30 18:52:39 +0900

    content and bg changed

--- my-project/src/app/page.tsx
+++ my-project/src/app/page.tsx
@@ -2,26 +2,47 @@
 export default function Home() {
   return (
   
-  <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-2 pb-20 gap-2 font-[family-name:var(--font-geist-sans)]">
+  <div className="bg-[url('/bl_land_bg_2.svg')] bg-cover bg-top bg-fixed grid grid-rows-[20px_1fr_20px] justify-items-center min-h-400 p-2 pb-20 gap-2 font-[family-name:var(--font-geist-sans)]">
     <div className="flex justify-around">
-      <main className="flex flex-col md:gap-[32px] mx-auto row-center-2 items-center sm:items-center">
-        <h1 className=" text-3xl  md:text-6xl md:mt-6 font-extralight font-Oxygen">РАМЕЙКОВ ИЛЬЯ ВАЛЕРЬЕВИЧ</h1>
-        <div className="md:container md:mx-auto flex-row columns-sm text-3xl md:space-y-2">
-            <div><img
-            src="\bl_land_bg.svg"
+      <main className="flex flex-col md:gap-[12px] mx-auto row-center-2 items-center sm:items-center">
+        <h1 className=" text-4xl  md:text-6xl md:mt-6 font-extralight font-Oxygen text-shadow-lg/50">РАМЕЙКОВ ИЛЬЯ ВАЛЕРЬЕВИЧ</h1>
+        <div className=" md:container columns-sm  md:space-y-2">
+            <div className="flex-col space-y-4 my-4 text-2xl md:h-220 text-right"><img className="md:h-180 h-120"
+            src="\photo.svg"
             alt="Producer"
           /></div>
-          <div className="md:h-12"></div>
-          <div className="flex-col space-y-4 my-4 md:w-160 text-right">
-          <div className="font-extralight font-Raleway">ПРОДЮСИРОВАНИЕ ИНСТАГРАМ
-МОНТАЖ, СЪЁМКА ВИДЕО</div>
-          <div className="font-extralight font-Raleway">ПРОДВИЖЕНИЕ
-СТОМАТОЛОГОВ
-И ЗУБНЫХ ТЕХНИКОВ</div>
-          <div className="font-extralight font-Raleway">РАЗВИЛ ИНСТАГРАМ
-С НУЛЯ ДО 215.000 АУДИТОРИИ</div>
-        </div>
-        <div className="flex gap-4 items-center my-6 md:my-12 flex-col md:ml-12 sm:flex-row">
+<div className="flex-col space-y-4 my-4 text-2xl md:w-160 text-right">
+<div className="font-extralight font-Raleway text-3xl text-shadow-lg/50">Развивайте свои идеи и бизнес-проекты</div>
+<div className="font-extralight font-Raleway text-shadow-lg/50">Профессиональный продюссинг и развитие проектов, которые меняют рынок</div>
+<div className="font-extralight font-Raleway">Я — профессиональный продюссер с более чем 10-летним опытом в развитии бизнес-проектов, 
+  инновационных идей и стратегий роста. Помогаю предпринимателям, стартапам и компаниям создавать и масштабировать успешные проекты, 
+  выходя за рамки стандартных решений</div>
+  </div>
+  <div className="font-extralight font-Raleway text-3xl text-center text-shadow-lg/50">ОПЫТ РАБОТЫ</div>
+<div className="flex-col space-y-4 my-4 text-2xl md:w-160 text-left">
+<div className="font-extralight font-Raleway">Продюсирование инстаграм</div>
+<div className="font-extralight font-Raleway">Съёмка видео</div>
+<div className="font-extralight font-Raleway">Продвижение стоматологов и зубных техников</div>
+<div className="font-extralight font-Raleway">Развил инстаграм с нуля до 215.000 аудитории</div></div>
+<div className="font-extralight font-Raleway  text-3xl text-center text-shadow-lg/50">ЧТО ВЫ ПОЛУЧИТЕ</div>
+<div className="flex-col space-y-4 my-4 text-2xl md:w-160 text-right">
+<div className="font-extralight font-Raleway">Индивидуальный подход к развитию вашего проекта</div>
+<div className="font-extralight font-Raleway">Стратегии по запуску и масштабированию бизнеса</div>
+<div className="font-extralight font-Raleway">Консультации по инновациям и оптимизации процессов</div>
+<div className="font-extralight font-Raleway">Поддержка в создании эффективных бизнес-моделей и маркетинговых решений, развитие соцсетей</div>
+</div>
+</div>
+<div className="font-extralight font-Raleway text-3xl text-center text-shadow-lg/50">ПОЧЕМУ ВЫБИРАЮТ МЕНЯ</div>
+<div className="flex-col space-y-4 my-4 text-2xl md:w-160 text-left">
+<div className="font-extralight font-Raleway">Опыт и профессионализм: сотни успешно реализованных проектов и стратегий</div>
+<div className="font-extralight font-Raleway">Креативность: нестандартный подход к развитию бизнес-идей</div>
+<div className="font-extralight font-Raleway">Гарантия результата: доведу ваш проект до новых высот</div>
+<div className="font-extralight font-Raleway">Гибкие условия сотрудничества</div>
+</div>
+<div className="font-extralight font-Raleway text-3xl text-left text-shadow-lg/50">ОТЗЫВЫ КЛИЕНТОВ</div>
+        
+        
+        <div className="flex gap-4 items-center my-2 md:my-2 flex-col md:ml-12 sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background text-sm gap-2 hover:bg-[#eabaa6] dark:hover:bg-[#ccc] font-Raleway  sm:text-xl h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="https://uslugi.yandex.ru/profile/IlyaRamejkov-3035849"
@@ -31,7 +52,7 @@ export default function Home() {
             ОСТАВИТЬ ЗАЯВКУ
           </a>
         </div>
-        </div>
+        
       </main>
       </div>
     </div>
